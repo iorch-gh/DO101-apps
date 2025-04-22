0d452d366938f742c4a3e6a97b9d05d61d951eac
Jorge Gonzalez Medina <jo.gonzalez24@info.uas.edu.mx>
2025-04-21 21:48:31 -0400

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars, Jorge González Medina!\n');
+});
+
+app.get('/pluton', function (req, res) {
+  res.send('Hello Pluton, Jorge González Medina!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
